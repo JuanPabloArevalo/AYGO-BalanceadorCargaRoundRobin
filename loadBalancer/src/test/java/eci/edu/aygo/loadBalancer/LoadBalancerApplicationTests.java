f331f1afdda01274bd2fe99bb888e40d151696c3
Juan Pablo Arevalo <juan.arevalo.merchan@hotmail.com>
2020-10-03 00:28:48 -0500

    Balanceador de carga

---
+++ loadBalancer/src/test/java/eci/edu/aygo/loadBalancer/LoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.aygo.loadBalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
